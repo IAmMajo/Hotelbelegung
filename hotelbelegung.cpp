8761302658f74c25c7100a78f13dfd1d9357c70f
Majo <max.j@overlack-online.de>
2020-06-18 15:42:32 +0200

    Added global array rooms

--- hotelbelegung.cpp
+++ hotelbelegung.cpp
@@ -2,6 +2,8 @@ using namespace std;
 
 #include <windows.h>
 
+bool rooms [6] [10]; 
+
 int main() {
   /* Änderung der von der Konsole benutzten Zeichenkodierung zu UTF-8, damit
   alle Zeichen richtig dargestellt werden. */
